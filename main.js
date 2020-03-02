355c0c49e1238cc868e336b0a5ed048dc10eebc9
anzhelaAbitova <55100446+anzhelaAbitova@users.noreply.github.com>
2020-03-02 17:51:55 +0300

    update

---
+++ main.js
@@ -0,0 +1,4 @@
+/* Значения из текстовых инпутов */
+const totalCost = document.getElementById('total-cost');
+    anInitialFee = document.getElementById('an-initial-fee');
+    anInitialFee = document.getElementById('total-cost');
