0978d474812ec74c3f5a98ab74a135b062f63953
anzhelaAbitova <55100446+anzhelaAbitova@users.noreply.github.com>
2020-03-03 21:22:30 +0300

    update

--- main.js
+++ main.js
@@ -51,4 +51,56 @@ const banks = [
 ]
 
 let currentPrecent = banks[0].precents;
-console.log(banks[0].precents);
+
+for (let bank of bankBtns) {
+    bank.addEventListener('click', () => {
+        for(let item of bankBtns) {
+            item.classList.remove('active');
+        }
+        bank.classList.add('active');
+        takeActiveBank(bank);
+    })
+}
+
+const takeActiveBank = currentActive => {
+    const dataAttrValue = currentActive.dataset.name;
+    const currentBank = banks.find(bank => bank.name === dataAttrValue);
+    currentPrecent = currentBank.precents;
+    calculation(totalCost.value, anInitialFee.value, creidtTerm.value);
+}
+
+for (let input of inputsRange) {
+    input.addEventListener('input', () => {
+        assignValue();
+        calculation(totalCost.value, anInitialFee.value, creidtTerm.value);
+    })
+}
+
+const calculation = (totalCost = 0, anInitialFee = 100000, creidtTerm = 1) => {
+    /* 
+        ЕП - Ежемесячный платеж
+        РК - Размер кредита
+        ПС - Процентная ставка
+        КМ - Количество месяцев
+
+        ЕП = (РК + (((РК / 100) * ПС) / 12) * КМ) / КМ;
+    */
+    let monthlyPayment; // Ежемесячный платеж
+    let loanAmount = totalCost - anInitialFee; // Размер кредита
+    let interestRate = currentPrecent; // Процентная ставка
+    let numberOfYears = creidtTerm; // Количество лет
+    let numberOfMonths = 12 * numberOfYears; // Количество месяцев
+
+    monthlyPayment = (loanAmount + (((loanAmount / 100) * interestRate) / 12) * numberOfMonths) / numberOfMonths;
+    const monthlyPaymentArounded = Math.round(monthlyPayment);
+    
+    if (monthlyPaymentArounded < 0) {
+        return false;
+    } 
+    else {
+        totalAmountOfCredit.innerHTML = `${loanAmount} ₽`;
+        totalMonthlyPayment.innerHTML = `${monthlyPaymentArounded} ₽`;
+        totalRecommendedIncome.innerHTML = `${monthlyPaymentArounded + ((monthlyPaymentArounded / 100) * 35)} ₽`;
+    }
+
+}
